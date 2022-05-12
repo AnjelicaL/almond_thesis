cc8398f41871f7730aa62a15843520db5f90a33d
Anjelica Leconte <anjelica.leconte@inrae.fr>
2022-05-12 17:29:29 +0200

    ajout package here

---
+++ packages_use.R
@@ -0,0 +1,17 @@
+
+  ##########################
+  ## Packages nécessaires ##
+  ##########################
+getwd()
+#install.packages(c("lubridate", "magrittr","tidyverse", "anytime", "heatmaply","ade4", "ggpubr", "rstatix"))
+  
+library("lubridate")
+library("magrittr")
+library("tidyverse")
+library("anytime")
+library("dplyr")
+library("ggpubr")
+library("rstatix")
+library("heatmaply")
+library("ade4")
+
